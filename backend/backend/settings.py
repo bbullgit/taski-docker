b603c6d8be273b42208c2a0131033b86b8e34df7
Максим Чепурнов <bbull11@yandex.ru>
2024-11-18 16:49:08 +0300

    add gateway

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['89.169.163.246', 'yapracttaski.ddns.net']
 
 
 # Application definition
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'collected_static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
